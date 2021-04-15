19965d5eddf60af3d152e6537240768b8335a005
Danny <mezenov.201521@gmail.com>
2021-04-15 14:54:11 +0300

    Интеграция с Service Discovery на основе Eureka

--- fw/src/main/java/com/devilpanda/task/fw/TaskServiceApplication.java
+++ fw/src/main/java/com/devilpanda/task/fw/TaskServiceApplication.java
@@ -3,11 +3,15 @@ package com.devilpanda.task.fw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.devilpanda.task")
+@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.devilpanda.task.adapter.jpa")
 @EntityScan(basePackages = "com.devilpanda.task.domain")
 public class TaskServiceApplication {
